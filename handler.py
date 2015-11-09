b8b2ed7fff3c4cabd3653e23e70cc01eb170fe09
Vladimir Petrigo <taenaru@gmail.com>
2015-11-09 14:03:14 +0300

    init commit

---
+++ handler.py
@@ -0,0 +1,27 @@
+import socketserver
+
+class CEL_UDP_Handler(socketserver.BaseRequestHandler):
+    '''
+    Test handler for UDP messages from CEL's ZigBee-Ethernet gateway
+    which sends datagram messages. This script must be started on the server
+    machine which will be parse incoming data
+    '''
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print("{}:{} wrote:".format(self.client_address[0],
+                                    self.client_address[1]))
+        spl_data = self.parse_data(data)
+        print('{} sent value {}'.format(spl_data[0], spl_data[1]))
+
+    def parse_data(self, data):
+        data_str = data.decode('utf-8')
+        splited_data = [x.strip() for x in data_str.split(':')]
+
+        return splited_data
+
+if __name__ == "__main__":
+    PORT = 5555
+    localserver = ("192.168.22.225", PORT)
+    server = socketserver.UDPServer(localserver, CEL_UDP_Handler)
+    server.serve_forever()
