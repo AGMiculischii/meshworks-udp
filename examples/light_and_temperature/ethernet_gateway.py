94853e75d140ec9d9aaf605de213b2686be4f325
Vladimir Petrigo <taenaru@gmail.com>
2015-11-09 14:53:10 +0300

    Add example scripts for MeshWorks gateway and sensor nodes This example helps to understand working flow of the MeshWorks network with a user server application.
    Such test environment represents:
    * Ethernet gateway ZMW-GW-ETH-1 (ethernet-gateway.py)
    * 2 x sensor nodes ZMW-SENSOR-1 (node1.py and node2.py)
    
    Each sensor node periodically sends information about push button status and
    temperature which is acquired from the internal Si7013 sensor.
    When gateway gets such message it will make UDP message and send to the user
    server.
    
    After the server gets that message it will pass it to the console output

---
+++ examples/light_and_temperature/ethernet_gateway.py
@@ -0,0 +1,48 @@
+# green LED is on GPIO PA6
+greenLed = ["greenLed", "PA6", "digital", "grLedF", 1]
+greenValues = ["discrete", 2, "off", "on"]
+
+# red LED is on GPIO PA7
+redLed = ["redLed", "PA7", "digital", "rdLedF", 1]
+redValues = ["discrete", 2, "off", "on"]
+
+# button is on GPIO PA3
+buttonPoint = ["button", "PA3", "digital", "buttonF", 1]
+buttonValues = ["discrete", 2, "up", "down"]
+
+# Конфигурация шлюза
+celPy.ApplicationName = "MeshWorks"
+celPy.DeviceName = "Gateway"
+celPy.IsSleepyDevice = False
+celPy.DataCollectionPoints = [buttonPoint]
+celPy.DataCollectionValues = [buttonValues]
+celPy.ControlPoints = [greenLed, redLed]
+celPy.ControlValues = [greenValues, redValues]
+
+# Обработка сообщений от других сетевых устройств
+def cpCallbackDataPointMessageReceived(deviceName, datapointName, discreteValueString, rangeValue):
+    if (deviceName == "Sensor 1"):
+        if (datapointName == "button"):
+            udpPayload = deviceName
+            udpPayload = (udpPayload + ": button=")
+            udpPayload = (udpPayload + rangeValue)
+            udp.send("192.168.22.225", 5555, udpPayload)
+        if (datapointName == "tempSensor"):
+            udpPayload = deviceName
+            udpPayload = (udpPayload + ": temp=")
+            udpPayload = (udpPayload + rangeValue)
+            udp.send("192.168.22.225", 5555, udpPayload)
+    if (deviceName == "Sensor 2"):
+        if (datapointName == "button"):
+            udpPayload = deviceName
+            udpPayload = (udpPayload + ": button=")
+            udpPayload = (udpPayload + rangeValue)
+            udp.send("192.168.22.225", 5555, udpPayload)
+        if (datapointName == "tempSensor"):
+            udpPayload = deviceName
+            udpPayload = (udpPayload + ": temp=")
+            udpPayload = (udpPayload + rangeValue)
+            udp.send("192.168.22.225", 5555, udpPayload)  
+
+def main():
+    pass
---
+++ examples/light_and_temperature/node1.py
@@ -0,0 +1,68 @@
+# Светодиод на выводе PA6 
+greenLed = ["greenLed", "PA6", "digital", "grLedF", 1]
+greenValues = ["discrete", 2, "off", "on"]
+# Светодиод на выводе PA7 
+redLed = ["redLed", "PA7", "digital", "rdLedF", 1]
+redValues = ["discrete", 2, "off", "on"]
+  
+# Data Point для кнопки
+button = ["button", "PB6", "digital", "buttonF", 1] 
+bValues = ["discrete", 2, "up", "down"] 
+
+prevButtonValue = 0
+buttonTickCount = 0 
+  
+def buttonF():
+    value = readDigital()
+    # Периодическая отправка данных
+    # о состоянии выключателя
+    if (buttonTickCount > 30):
+        buttonTickCount = 0
+        sendDataReport(value, "button state")
+    # если значение поменялось 
+    if (value != prevButtonValue):  
+        sendDataReport(value, "button state")
+        # Устанавливаем состояние светодиода
+        # в соответствии с состоянием выключателя
+        celPy.AdjustLocalControlPoint("greenLed", value)    
+    prevButtonValue = value
+    buttonTickCount = (buttonTickCount + 1)
+    
+# Data Point для датчика температуры
+tempSensor = ["tempSensor", "PA1", "i2c", "tempMeasFunc", 20]
+tempSensorValues = ["range", -40, 120]
+
+# Чтение температуры с Si7013
+def tempMeasFunc():
+    value = readI2c(0x41, 0xE3, bigEndian)
+    # Преобразование в градусы Цельсия
+    value = (value * 175)
+    value = (value / 65535)
+    value = (value - 47)
+    sendDataReport(value, "degrees C")
+
+celPy.addTickFunction(heartbeatLed, 20) 
+  
+ledState = 0
+  
+def heartbeatLed(): 
+    if (ledState == 0): 
+        celPy.AdjustLocalControlPoint("redLed", 1)
+        ledState = 1
+        return
+    if (ledState == 1): 
+        celPy.AdjustLocalControlPoint("redLed", 0)
+        ledState = 0
+  
+####################################################
+# Сетевые настройки
+celPy.ApplicationName = "MeshWorks"  
+celPy.DeviceName = "Sensor 1" 
+celPy.IsSleepyDevice = False
+celPy.DataCollectionPoints = [button, tempSensor] 
+celPy.DataCollectionValues = [bValues, tempSensorValues]
+celPy.ControlPoints = [greenLed, redLed]
+celPy.ControlValues = [greenValues, redValues] 
+  
+def main(): 
+    pass
